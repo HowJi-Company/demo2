2c57e6a7b238fa338ca2b6d9a03b39c8e8fa2614
How Huang <99330165+Howwoh1996@users.noreply.github.com>
2025-12-17 18:08:50 +0800

    update

---
+++ assets/button_下一步-Dza0cO2-.js
@@ -0,0 +1,1 @@
+const t="/images/button_%E4%B8%8B%E4%B8%80%E6%AD%A5.png";export{t as _};
---
+++ assets/message-CKVmI11u.js
@@ -0,0 +1,76 @@
+import{A as y,r as e}from"./index-C5k5Gm4v.js";const v=y("message",()=>{const o=e({pairings:[{adjectives:["熱情","真誠"],content:`最愛跟你交心暢談
+八卦小事聊一整天
+心情就能快速充電
+
+Happy Holidays
+謝謝我的活力小太陽`},{adjectives:["熱情","可靠"],content:`謝謝你一直carry我
+有你在真的很罩
+讓人覺得好幸福~
+
+Stay Cheerful
+今年也請多多指教囉!`},{adjectives:["熱情","細心"],content:`你看起來熱情四射
+但心思又很細膩
+謝謝一直以來的照顧
+
+Stay Cheerful
+祝你永遠閃閃發光,`},{adjectives:["熱情","獨特"],content:`一直都覺得你好特別
+只要跟你相處的時候
+就能被感染好心情
+
+Happy Holidays
+期待未來一起瘋玩!,`},{adjectives:["熱情","幽默"],content:`你其實真的有點好笑
+總讓我哭笑不得
+平凡的日子都變得有趣
+
+Happy Holidays
+祝你的幽默繼續升級,`},{adjectives:["真誠","可靠"],content:`一路上多虧有你陪著
+幫助了我好多時候
+跟你相處超級踏實
+
+Stay Cheerful
+祝所有好事都黏你❤,`},{adjectives:["真誠","細心"],content:`謝謝你悄悄陪著我
+連我沒說的情緒都懂
+真的每次都超感動~
+
+Happy Holidays
+祝你擁有一切好運!,`},{adjectives:["真誠","獨特"],content:`好愛你的與眾不同
+有你在就能聊好多秘密
+謝謝一直以來的陪伴
+
+Happy Holidays
+未來繼續合作愉快!,`},{adjectives:["真誠","幽默"],content:`你知道嗎?你超有魅力
+跟你一起玩的時候
+偶爾的幽默超對我胃口
+
+Stay Cheerful
+今天想說聲謝謝你❤,`},{adjectives:["可靠","細心"],content:`謝謝你總是carry全場
+有你在我背後
+就能放下120萬顆心
+
+Stay Cheerful
+年末記得好好放鬆,`},{adjectives:["可靠","獨特"],content:`真的欠你很多人情
+謝謝你一直幫助我
+有你在每天都不無聊
+
+Happy Holidays
+今後也要靠你啦~,`},{adjectives:["可靠","幽默"],content:`難得有人可靠又幽默
+根本人間稀有種
+能認識你是我福氣!
+
+Stay Cheerful
+祝你未來天天開心,`},{adjectives:["細心","獨特"],content:`歡聚的節日就想到你
+獨特的個性天天帶來驚喜
+跟你相處充滿開心回憶
+
+Happy Holidays
+謝謝你的細心照顧,`},{adjectives:["細心","幽默"],content:`最喜歡跟你相處了
+細心又幽默的你
+總讓小事都變得好玩
+
+Happy Holidays
+祝你心願通通實現!,`},{adjectives:["獨特","幽默"],content:`四次元的你
+總有意想不到的笑點
+好喜歡你的獨特魅力
+
+Happy Holidays
+一起收集更多回憶吧!,`}]}),t=e(""),d=n=>{t.value=n},a=e(""),r=n=>{a.value=n},s=e(""),i=n=>{s.value=n},c=e("");return{cardMessages:o,selectedMsg:s,setSelectedMsg:i,setTakerName:d,setGiverName:r,cardTakerName:t,cardGiverName:a,cardBase64Url:c,setCardBase64Url:n=>{c.value=n}}});export{v as u};
